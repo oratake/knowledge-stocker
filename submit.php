d4d494e38293eddc52b37b9a4758041e7c307b5e
oratake <smith.shimomura@gmail.com>
2020-12-22 08:30:23 +0900

    refactor: POSTメソッドではないときに早めにexit

--- submit.php
+++ submit.php
@@ -1,7 +1,7 @@
 <?php
-	if($_SERVER['REQUEST_METHOD'] === 'POST'){
-		echo 'タイトル: '.$_POST['title']."\n";
-		echo '内容: '.$_POST['article'];
-	} else {
+	if($_SERVER['REQUEST_METHOD'] !== 'POST'){
 		echo 'POSTではないです';
+		exit;
 	}
+	echo 'タイトル: '.$_POST['title']."\n";
+	echo '内容: '.$_POST['article'];
