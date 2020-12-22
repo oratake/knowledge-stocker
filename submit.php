7f62317edb7f640368bae1dd3d02ada3ccbf06fe
oratake <smith.shimomura@gmail.com>
2020-12-22 22:33:02 +0900

    add: POSTのバラメータが空ならecho

--- submit.php
+++ submit.php
@@ -3,5 +3,11 @@
 		echo 'POSTではないです';
 		exit;
 	}
+
+	if($_POST['title'] === '' || $_POST['article'] === ''){
+		echo 'POSTが空です';
+		exit;
+	}
+
 	echo 'タイトル: '.$_POST['title']."\n";
 	echo '内容: '.$_POST['article'];
