c248df63d52ef66ee1579c41068793b6e49bf82c
oratake <smith.shimomura@gmail.com>
2020-12-23 08:32:27 +0900

    add: 投稿を保存する関数のガワ

--- submit.php
+++ submit.php
@@ -1,4 +1,5 @@
 <?php
+
 	if($_SERVER['REQUEST_METHOD'] !== 'POST'){
 		echo 'POSTではないです';
 		exit;
@@ -9,5 +10,13 @@
 		exit;
 	}
 
+	// 記事の投稿
+	submitArticle($_POST['title'], $_POST['article']);
+
 	echo 'タイトル: '.$_POST['title']."\n";
 	echo '内容: '.$_POST['article'];
+
+	function submitArticle($title, $article)
+	{
+		return true;
+	}
