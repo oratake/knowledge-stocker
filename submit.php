5932f62cc5a225332fe9730660106bfc673c09f3
oratake <smith.shimomura@gmail.com>
2020-12-24 08:06:32 +0900

    add: 記事を書き込む

--- submit.php
+++ submit.php
@@ -12,11 +12,31 @@
 
 	// 記事の投稿
 	submitArticle($_POST['title'], $_POST['article']);
+	echo '書き込めました';
+	exit;
 
-	echo 'タイトル: '.$_POST['title']."\n";
-	echo '内容: '.$_POST['article'];
+	// echo 'タイトル: '.$_POST['title']."\n";
+	// echo '内容: '.$_POST['article'];
 
 	function submitArticle($title, $article)
 	{
-		return true;
+		$filename = 'test_article.csv';
+
+		if(is_writable($filename)){
+			// fopenは開けないとき(bool)falseが返る
+			$article_filepointer = fopen($filename, 'a');
+			if($article_filepointer === false){
+				echo 'ファイルが開けませんでした';
+				exit;
+			}
+
+			$line = $title.','.$article;
+			fwrite($article_filepointer, $line."\n");
+			fclose($article_filepointer);
+
+			return true;
+		} else {
+			echo '書き込めませんでした';
+			exit;
+		}
 	}
