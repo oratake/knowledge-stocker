63082ee3add49738db4da40635a5f43a45634268
oratake <smith.shimomura@gmail.com>
2020-12-25 08:16:34 +0900

    add: 記事を取得する関数 + インデントをtabに修正

--- index.php
+++ index.php
@@ -1,42 +1,58 @@
 <?php
-    $hello = 'Hello, world.';
+	$hello = 'Hello, world.';
+
+	$articles = getArticles();
+
+	function getArticles()
+	{
+		return [
+			[
+				'title' => 'タイトル1',
+				'article' => '記事1'
+			],
+			[
+				'title' => 'タイトル2',
+				'article' => '記事2'
+			],
+		];
+	}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title><?= $hello; ?></title>
+	<meta charset="utf-8">
+	<title><?= $hello; ?></title>
 </head>
 <body>
-    <h1><?= $hello; ?></h1>
-    <div id="posts">
-        <h2>一覧</h2>
-        <div>
-            <h3>title 1</h3>
-            <p>
-                text is here
-            </p>
-        </div>
-        <div>
-            <h3>title 2</h3>
-            <p>
-                text is here
-            </p>
-        </div>
-    </div>
+	<h1><?= $hello; ?></h1>
+	<div id="posts">
+		<h2>一覧</h2>
+		<div>
+			<h3>title 1</h3>
+			<p>
+				text is here
+			</p>
+		</div>
+		<div>
+			<h3>title 2</h3>
+			<p>
+				text is here
+			</p>
+		</div>
+	</div>
 
-    <div id="form">
-        <form action="./submit.php" method="POST">
-            <div>
-                <label for="title">タイトル</label>
-                <input type="" name="title">
-            </div>
-            <div>
-                <label for="article">記事</label>
-                <textarea name="article"></textarea>
-            </div>
-            <input type="submit" value="送信">
-        </form>
-    </div>
+	<div id="form">
+		<form action="./submit.php" method="POST">
+			<div>
+				<label for="title">タイトル</label>
+				<input type="" name="title">
+			</div>
+			<div>
+				<label for="article">記事</label>
+				<textarea name="article"></textarea>
+			</div>
+			<input type="submit" value="送信">
+		</form>
+	</div>
 </body>
 </html>
